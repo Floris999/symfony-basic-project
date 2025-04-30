83e50e07f0fdcb507d0de3e5397318264089c95f
Floris999 <florisvandinteren@gmail.com>
2025-04-30 14:41:47 +0200

    general commit

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,14 +2,25 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController
+class MainController extends AbstractController
 {
     #[Route('/')]
     public function index(): Response
     {
-        return new Response('<strong>Starshop</strong>: your monopoly-busting option for Starship parts!');
+        $starshipCount = 457;
+        $myship = [
+            'name' => 'USS LeafyCruiser (NCC-0001)',
+            'class' => 'Garden',
+            'captain' => 'Jean-Luc Pickles',
+            'status' => 'under construction',
+        ];
+        return $this->render('main/index.html.twig', [
+            'numberOfStarships' => $starshipCount,
+            'myShip' => $myship,
+        ]);
     }
 }
