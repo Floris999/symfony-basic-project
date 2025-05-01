6bda3be4d84286abc6c8056d765f88e2fda50a2e
Floris999 <florisvandinteren@gmail.com>
2025-05-01 11:24:40 +0200

    general commit

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\StarshipRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,18 +10,13 @@ use Symfony\Component\Routing\Attribute\Route;
 class MainController extends AbstractController
 {
     #[Route('/')]
-    public function index(): Response
+    public function index(StarshipRepository $starshipRepository): Response
     {
-        $starshipCount = 457;
-        $myship = [
-            'name' => 'USS LeafyCruiser (NCC-0001)',
-            'class' => 'Garden',
-            'captain' => 'Jean-Luc Pickles',
-            'status' => 'under construction',
-        ];
+        $ships = $starshipRepository->findAll();
+        $myShip = $ships[array_rand($ships)];
         return $this->render('main/index.html.twig', [
-            'numberOfStarships' => $starshipCount,
-            'myShip' => $myship,
+            'myShip' => $myShip,
+            'ships' => $ships,
         ]);
     }
 }
---
+++ src/Controller/StarshipApiController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use
+    Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Psr\Log\LoggerInterface;
+use App\Model\Starship;
+use App\Repository\StarshipRepository;
+
+#[Route('/api/starships')]
+class StarshipApiController extends AbstractController
+{
+    #[Route('', methods: ['GET'])]
+    public function getCollection(StarshipRepository $repository): Response
+    {
+        $starships = $repository->findAll();
+        return $this->json($starships);
+    }
+
+    #[Route('/{id<\d+>}', methods: ['GET'])]
+    public function get(int $id, StarshipRepository $repository): Response
+    {
+        $starship = $repository->find($id);
+        if (!$starship) {
+            throw $this->createNotFoundException('Starship not found');
+        }
+        return $this->json($starship);
+    }
+}
